dd3d0d5a7be211107991ce1076d05f12faad08fd
Eric Gowens <eric.gowens@gmail.com>
2014-10-19 15:52:15 +0200

    Live creation working

--- app/controllers/live_collection_controller.rb
+++ app/controllers/live_collection_controller.rb
@@ -3,22 +3,23 @@ class LiveCollectionController < ApplicationController
   around_filter :shopify_session
 
   def new
-    @live = LiveCollection.new
+    @live_collection = LiveCollection.new
   end
 
   def create
-    @live = LiveCollection.new(live_params)
-    @shop_live_collection = ShopifyAPI::CustomCollection.new("title" => @live.title)
+    @live_collection = LiveCollection.new(live_params)
+    @shop_live_collection = ShopifyAPI::CustomCollection.new("title" => @live_collection.title)
 
-    if @live.save
+    if @live_collection.save
 
       #save collection to shopify store
-      @shop_collection.save
+      #REARRANGE SO THAT COLLECTION IS NOT SAVED IN SHOPIFY IF NO PRODUCTS ARE FOUND ***************************
+      @shop_live_collection.save
       flash[:success] = "New Live Collection Created!"
 
       #calculate afterdate
-      num = @live.time_num
-      case @live.time_type
+      num = @live_collection.time_number
+      case @live_collection.time_type
         when "days"
           date = Time.now - num.days
         when "weeks"
@@ -32,17 +33,26 @@ class LiveCollectionController < ApplicationController
       #get applicable products for collection
       @included_products = ShopifyAPI::Product.where(:published_at_min => date)
 
-      #Add applicable products to the new collection
-      @included_products.each do |prod|
-        collect = ShopifyAPI::Collect.new("collection_id" => @shop_collection.id, "product_id" => prod.id)
-        collect.save!
+      if @included_products != nil
+        #Add applicable products to the new collection
+        @included_products.each do |prod|
+          collect = ShopifyAPI::Collect.new("collection_id" => @shop_live_collection.id, "product_id" => prod.id)
+          collect.save!
+        end
+      else
+        flash[:alert] = "No products found"
+        @live_collection.delete
       end
+
+      redirect_to '/'
+    else
+      render 'live_collection/new'
     end
   end
 
   private
 
   def live_params
-    params.require(:live).permit(:shop_url, :title, :time_num, :time_type)
+    params.require(:live_collection).permit(:shop_url, :title, :time_number, :time_type)
   end
 end
